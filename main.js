873ead2d631e9ceae6e936b2b0bf8fee6cb6a61d
4atatime <lexieyu42yce@gmail.com>
2023-04-30 18:02:30 +0200

    share&instagram buttons; footer; random text;

--- main.js
+++ main.js
@@ -38,3 +38,17 @@ $(function() {
    });
  
  });
+
+ $('#sharebutton').on('click', () => {
+   if (navigator.share) {
+     navigator.share({
+         title: 'Web Share API Draft',
+         text: 'Take a look at this spec!',
+         url: 'https://wicg.github.io/web-share/#share-method',
+       })
+       .then(() => console.log('Successful share'))
+       .catch((error) => console.log('Error sharing', error));
+   } else {
+     console.log('Share not supported on this browser, do it the old way.');
+   }
+ });
